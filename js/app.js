c4b97716796b87a4abca88116fea72644a302372
thedteam <thedteam18@gmail.com>
2021-12-02 15:30:32 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -37,7 +37,20 @@ const coffees = [
     image: "images/coffee9.jpg"
   }
 ];
-
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  );
+  container.innerHTML = output;
+};
 
 document.addEventListener("DOMContentLoaded", showCoffees);
 
